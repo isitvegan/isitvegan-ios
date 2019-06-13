bdb558cefe94714f078e3dedd999fe20052ffb03
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-06-13 19:53:43 +0200

    Set up split view controller

--- isitvegan/RootViewController.swift
+++ isitvegan/RootViewController.swift
@@ -1,8 +1,16 @@
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UISplitViewController, UISplitViewControllerDelegate {
     override func viewDidLoad() {
-        super.viewDidLoad()
+        self.delegate = self
+        self.preferredDisplayMode = .allVisible
+    }
+
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController) -> Bool {
+        // Return true to prevent UIKit from applying its default behavior
+        return true
     }
 }
-
