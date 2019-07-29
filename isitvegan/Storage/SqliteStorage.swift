721689534ed598d6c827de2c4d81d1583c9130e9
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-07-29 09:59:40 +0200

    Put names into their own FTS table

--- isitvegan/Storage/SqliteStorage.swift
+++ isitvegan/Storage/SqliteStorage.swift
@@ -4,49 +4,94 @@ import enum Swift.Result
 
 class SqliteStorage {
     let connection: Connection
-    
-    private let items = VirtualTable("items")
-    private let id = Expression<String>("id")
-    private let name = Expression<String>("name")
-    private let itemDescription = Expression<String>("description")
-    private let eNumber = Expression<String?>("eNumber")
-    private let state = Expression<String>("state")
-    private let rank = Expression<Int64>("rank")
-    
+
+    private let items = Table("items")
+    private let names = VirtualTable("names")
+    private let sources = Table("sources")
+
     init(connection: Connection) {
         self.connection = connection
     }
     
     func setupSchema() throws {
-        try createItemsTable()
+        try connection.transaction {
+            try createItemsTable()
+            try createNamesTable()
+            try createSourcesTable()
+        }
+    }
+
+    private func createNamesTable() throws {
+        let config = FTS5Config()
+            .column(Names.name)
+            .column(Names.item, [.unindexed])
+        try connection.run(names.create(.FTS5(config), ifNotExists: true))
     }
 
     private func createItemsTable() throws {
-        let config = FTS5Config()
-            .column(id, [.unindexed])
-            .column(name)
-            .column(itemDescription, [.unindexed])
-            .column(eNumber, [.unindexed])
-            .column(state, [.unindexed])
-        try connection.run(items.create(.FTS5(config), ifNotExists: true))
+        try connection.run(items.create(ifNotExists: true) { t in
+            t.column(Items.id, primaryKey: .autoincrement)
+            t.column(Items.name)
+            t.column(Items.itemDescription)
+            t.column(Items.eNumber)
+            t.column(Items.state)
+        })
+    }
+
+    private func createSourcesTable() throws {
+        try connection.run(sources.create(ifNotExists: true) { t in
+            t.column(Sources.id, primaryKey: .autoincrement)
+            t.column(Sources.item, references: items, Items.id)
+            t.column(Sources.type)
+            t.column(Sources.value)
+            t.column(Sources.lastChecked)
+        })
+    }
+}
+
+extension SqliteStorage {
+    private class Sources {
+        static let id = Expression<Int64>("id")
+        static let item = Expression<Int64>("item")
+        static let type = Expression<String>("type")
+        static let value = Expression<String>("value")
+        static let lastChecked = Expression<Date?>("lastChecked")
+    }
+
+    private class Items {
+        static let id = Expression<Int64>("id")
+        static let name = Expression<String>("name")
+        static let itemDescription = Expression<String>("description")
+        static let eNumber = Expression<String?>("eNumber")
+        static let state = Expression<String>("state")
+    }
+
+    private class Names {
+        static let name = Expression<String>("name")
+        static let item = Expression<Int64>("item")
+        static let rank = Expression<Int64>("rank")
     }
 }
 
 extension SqliteStorage: StorageReader {
     func getAllItems(limit: Int) throws -> StorageReaderResult {
-        return try findItemsBy(query: items.order(name), limit: limit)
+        return try findItemsBy(query: items.order(Items.name), limit: limit)
     }
 
     func findItems(eNumber eNumberQuery: String, limit: Int) throws -> StorageReaderResult {
         let escapedQuery = escapeLikeWildcard("E\(eNumberQuery)", escape: wildcardEscapeCharacter)
-        let wildcard = eNumber.like("\(escapedQuery)%", escape: wildcardEscapeCharacter)
-        let query = items.filter(wildcard).order(eNumber)
+        let wildcard = Items.eNumber.like("\(escapedQuery)%", escape: wildcardEscapeCharacter)
+        let query = items.filter(wildcard).order(Items.eNumber)
         return try findItemsBy(query: query, limit: limit)
     }
 
     func findItems(name nameQuery: String, limit: Int) throws -> StorageReaderResult {
         let quotedQuery = quoteMatchString(nameQuery)
-        let query = items.filter(name.match("\(quotedQuery)*")).order(rank)
+        let query = items.select(items[*])
+                         .join(names, on: names[Names.item] == items[Items.id])
+                         .filter(names[Names.name].match("\(quotedQuery)*"))
+                         .order(names[Names.rank])
+                         .group(items[Items.id])
         return try findItemsBy(query: query, limit: limit)
     }
 }
@@ -56,14 +101,8 @@ extension SqliteStorage: StorageWriter {
         return Result {
             try connection.transaction {
                 for item in items {
-                    let query = self.items.insert(
-                        self.id <- item.id,
-                        self.name <- item.name,
-                        self.itemDescription <- item.description,
-                        self.eNumber <- item.eNumber,
-                        self.state <- item.state.rawValue
-                    )
-                    try self.connection.run(query)
+                    let itemId = try writeItem(item)
+                    try writeItemNames(itemId: itemId, item: item)
                 }
             }
         }
@@ -71,24 +110,68 @@ extension SqliteStorage: StorageWriter {
 
     func deleteAllItems() -> Result<Void, Error> {
         return Result {
-            try connection.run(items.delete())
+            try connection.transaction {
+                try connection.run(items.delete())
+                try connection.run(names.delete())
+                try connection.run(sources.delete())
+            }
         }
     }
 }
 
 extension SqliteStorage {
-    private func findItemsBy(query: VirtualTable, limit: Int) throws -> StorageReaderResult {
+    private func writeItem(_ item: Item) throws -> Int64  {
+        let query = items.insert(
+            Items.name <- item.name,
+            Items.itemDescription <- item.description,
+            Items.eNumber <- item.eNumber,
+            Items.state <- item.state.rawValue
+        )
+        try connection.run(query)
+        return connection.lastInsertRowid
+    }
+
+    private func writeItemNames(itemId: Int64, item: Item) throws {
+        try writeItemName(itemId: itemId, name: item.name)
+        for name in item.alternativeNames {
+            try writeItemName(itemId: itemId, name: name)
+        }
+    }
+
+    private func writeItemName(itemId: Int64, name: String) throws {
+        try connection.run(names.insert(
+            Names.item <- itemId,
+            Names.name <- name
+        ))
+    }
+
+    private func writeItemSources(itemId: Int64, item: Item) throws {
+        for source in item.sources {
+            try writeItemSource(itemId: itemId, source: source)
+        }
+    }
+
+    private func writeItemSource(itemId: Int64, source: Item.Source) throws {
+        try connection.run(sources.insert(
+            Sources.item <- itemId,
+            Sources.type <- source.type.rawValue,
+            Sources.value <- source.value,
+            Sources.lastChecked <- source.lastChecked
+        ))
+    }
+
+    private func findItemsBy(query: SchemaType, limit: Int) throws -> StorageReaderResult {
         let itemRows = try connection.prepare(query.limit(limit)).map { $0 }
         let items = itemRows.map { itemFrom(row: $0) }
         return StorageReaderResult(items: items)
     }
 
     private func itemFrom(row: Row) -> Item {
-        Item(id: row[id],
-             name: row[name],
-             state: Item.State(rawValue: row[state])!,
-             eNumber: row[eNumber],
-             description: row[itemDescription],
+        Item(name: row[Items.name],
+             alternativeNames: [],
+             state: Item.State(rawValue: row[Items.state])!,
+             eNumber: row[Items.eNumber],
+             description: row[Items.itemDescription],
              sources: [])
     }
 
