35abaa174c9a2121f39996ac2612e39f8db9637f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-07-29 11:10:52 +0200

    Make internal methods private

--- isitvegan/Font.swift
+++ isitvegan/Font.swift
@@ -1,16 +1,17 @@
 import UIKit
 
 extension UIFont {
-    func withTraits(traits:UIFontDescriptor.SymbolicTraits) -> UIFont {
-        let descriptor = fontDescriptor.withSymbolicTraits(traits)
-        return UIFont(descriptor: descriptor!, size: 0) //size 0 means keep the size as it is
-    }
-
     func bold() -> UIFont {
         return withTraits(traits: .traitBold)
     }
 
     func italic() -> UIFont {
         return withTraits(traits: .traitItalic)
+    }
+
+    private func withTraits(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        let keepSizeAsIs: CGFloat = 0.0
+        let descriptor = fontDescriptor.withSymbolicTraits(traits)
+        return UIFont(descriptor: descriptor!, size: keepSizeAsIs)
     }
 }
